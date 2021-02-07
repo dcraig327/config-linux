70799ce4e7701501e91072f29b76786d485190db
David Craig <dcraig327@gmail.com>
2021-02-07 14:33:35 -0800

    update gpufan.sh

--- gpufan.sh
+++ gpufan.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
 #sleep 30 was used to make sure this starts after the Nvidia driver
 #but that's no longer needed after placing in .xprofile
 nvidia-settings -a "GPUFanControlState=1" > /dev/null 2>&1
  
 while true
 do
-    temp=`nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits`
+    temp=`nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits > gpufan.log 2>&1`
 
     fan=$((temp*3))
     fan=$((fan-95))
